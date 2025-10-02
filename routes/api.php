9b91eb48fdb270ba2ac200fe92e10c9aa910fb99
MichelleJimenez01 <alba_mjimenez@soy.sena.edu.co>
2025-10-02 17:00:37 -0500

    rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PublicationController;
+use App\Models\Category;
+use App\Models\Publication;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -17,5 +22,8 @@ Route::apiResource('users', UserController::class);
 Route::apiResource('roles', RoleController::class);
 Route::apiResource('profiles', ProfileController::class);
 Route::apiResource('messages', MessageController::class);
+Route::apiResource('publications', PublicationController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('notifications', NotificationController::class);
 
 
