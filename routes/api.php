3ef9b19b0db2b8cfbcfe54b30a00746e9c026668
MichelleJimenez01 <alba_mjimenez@soy.sena.edu.co>
2025-10-02 17:10:45 -0500

    confirmacion rutas

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,19 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PublicationController;
-use App\Models\Category;
-use App\Models\Publication;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MessageController;
-use App\Http\Controllers\AblyTestController;
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 
 Route::apiResource('users', UserController::class);
